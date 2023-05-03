01709ef0106946094fa3b386dadbb6be2f43f8f5
Abraao Lima <24abraao@gmail.com>
2023-05-03 20:08:06 -0300

    Analisador Lexico commit inicial

---
+++ parser.cpp
@@ -0,0 +1,24 @@
+#include "parser.h"
+#include "tokens.h"
+#include <iostream>
+using std::cout;
+
+void Parser::Start()
+{
+    // enquanto não atingir o fim da entrada
+    while ((lookahead = scanner.yylex()) != 0)
+    {
+        // trata o token recebido do analisador léxico
+        switch(lookahead)
+        {
+            case SOME: cout << "SOME\n"; break;
+            case ALL: cout << "ALL\n"; break;
+            case VALUE: cout << "VALUE\n"; break;
+            case AND: cout << "AND\n"; break;
+            case THAT: cout << "THAT\n"; break;
+            case CARDINAL: cout << "CARDINAL: " << scanner.YYText() << "\n";; break;
+            case PROPERTY: cout << "PROPERTY: " << scanner.YYText() << "\n";; break;
+            case CLASS: cout << "CLASS: " << scanner.YYText() << "\n"; break;
+        }
+    }
+}
---
+++ tokens.h
@@ -0,0 +1,2 @@
+// constantes para os tokens 
+enum {SOME=1, ALL, VALUE, MIN, MAX, EXACTLY, THAT, NOT, AND, OR, CLASS, PROPERTY, CARDINAL, NUM}; 
