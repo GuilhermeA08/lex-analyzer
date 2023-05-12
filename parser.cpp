4a3aae151f4d7f4f9cbc07a622b6f7118e280596
PauloVictor310 <paulovictorbema@gmail.com>
2023-05-12 14:38:10 -0300

    fix: cardinal

--- parser.cpp
+++ parser.cpp
@@ -43,26 +43,6 @@ void Parser::Start()
         if (symbolTable.find(scanner.YYText()) == symbolTable.end()) {
             symbolTable[scanner.YYText()] = {++tokenCounter, tokensName[lookahead]};
         }
-
-        // trata o token recebido do analisador léxico
-        switch(lookahead)
-        {
-            case SOME: cout << "SOME\n"; break;
-            case ALL: cout << "ALL\n"; break;
-            case VALUE: cout << "VALUE\n"; break;
-            case MIN: cout << "MIN\n"; break;
-            case MAX: cout << "MAX\n"; break;
-            case EXACTLY: cout << "EXACTLY\n"; break;
-            case THAT: cout << "THAT\n"; break;
-            case ONLY: cout << "ONLY\n"; break;
-            case NOT: cout << "NOT\n"; break;
-            case AND: cout << "AND\n"; break;
-            case OR: cout << "OR\n"; break;
-            case PARENTHESIS: cout << "PARENTHESIS\n"; break;
-            case CARDINAL: cout << "CARDINAL: " << scanner.YYText() << "\n"; break;
-            case PROPERTY: cout << "PROPERTY: " << scanner.YYText() << "\n"; break;
-            case CLASS: cout << "CLASS: " << scanner.YYText() << "\n"; break;
-        }
     }
 
     cout << "\nTabela de Símbolos\n";
